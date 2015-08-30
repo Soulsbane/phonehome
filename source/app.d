6d5b273225f7ab56645e817e85d99ded4fd3b1b5
Paul Crane <paul@acheronsoft.com>
2015-08-30 00:18:20 -0500

    Remove getPhoneBookEntry function and use struct intialization syntactic sugar instead.

--- source/app.d
+++ source/app.d
@@ -30,7 +30,7 @@ string pluralizeEntryCount(immutable uint count) pure @safe
 	return pluralizedNumber;
 }
 
-void processPhoneBookEntries(string phoneBookName, string searchTerm, bool allowMultipleEntries = false) @safe
+void processPhoneBookEntries(immutable string phoneBookName, immutable string searchTerm, bool allowMultipleEntries = false) @safe
 {
 	auto lines = loadPhoneBook(phoneBookName).lineSplitter();
 	uint entryCount = 0;
@@ -49,7 +49,7 @@ void processPhoneBookEntries(string phoneBookName, string searchTerm, bool allow
 			if(line.indexOf(searchTerm, CaseSensitive.no) != -1)
 			{
 				immutable string[] values = line.split(";");
-				PhoneBookEntry entry = getPhoneBookEntry(values);
+				PhoneBookEntry entry = { values[0],values[1], values[2], values[3], values[4] }; // FIXME: Maybe more size checking here before using values
 
 				entries ~= entry;
 				++entryCount;
@@ -103,36 +103,6 @@ string loadPhoneBook(immutable string phoneBookName) @safe
 	}
 
 	return text;
-}
-
-PhoneBookEntry getPhoneBookEntry(immutable string[] values) pure @safe
-{
-	PhoneBookEntry entry;
-	int i;
-
-	while(i < values.length)
-	{
-		final switch(i)
-		{
-			case 0:
-				entry.name = values[0];
-				break;
-			case 1:
-				entry.nickName = values[1];
-				break;
-			case 2:
-				entry.homeNumber = values[2];
-				break;
-			case 3:
-				entry.cellNumber = values[3];
-				break;
-			case 4:
-				entry.workNumber = values[4];
-				break;
-		}
-		++i;
-	}
-	return entry;
 }
 
 void main(string[] arguments)
